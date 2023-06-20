7d75102ad57582b80209302b291d287a519fd092
hsingla005 <hsingla@twilio.com>
2023-06-20 17:20:23 +0530

    FLEXY-4595 state update

--- ui-src/src/flex-hooks/actions/StartOutboundCall.ts
+++ ui-src/src/flex-hooks/actions/StartOutboundCall.ts
@@ -5,7 +5,7 @@ import AppState, { reduxNamespace } from '../../types/AppState';
 export default function applySelectedCallerIdForDialedNumbers(flex: typeof Flex, manager: Flex.Manager) {
   flex.Actions.addListener(`beforeStartOutboundCall`, async (payload, _abortFunction) => {
     const state = manager.store.getState() as AppState;
-    const { selectedCallerId } = state[reduxNamespace].outboundCallerIdSelector;
+    const { selectedCallerId } = state[reduxNamespace];
 
     if (!payload.callerId && selectedCallerId) payload.callerId = selectedCallerId;
   });
